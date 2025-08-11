e0ea79b8d5096cd2ce66d962e2bdf18d127f792a
Pratik Satta <NITRO V 15@192.168.1.74>
2025-08-11 22:38:50 +0545

    Added SE

--- My2DGame/src/tile_interactive/IT_DryTree.java
+++ My2DGame/src/tile_interactive/IT_DryTree.java
@@ -7,6 +7,7 @@ import main.GamePanel;
 
 public class IT_DryTree extends InteractiveTile{
 
+	GamePanel gp;
 	public IT_DryTree(GamePanel gp, int col, int row) {
 		super(gp,col,row);
 		this.gp = gp;
