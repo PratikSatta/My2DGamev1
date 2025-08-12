2490498a20a837f92398869ecfe9210412d39fc6
Pratik Satta <sattapratik01@gmail.com>
2025-08-12 17:46:33 +0545

    Added dialgoues and sleep parameter

--- My2DGame/src/monster/MON_SkeletonLord.java
+++ My2DGame/src/monster/MON_SkeletonLord.java
@@ -2,9 +2,11 @@ package monster;
 
 import java.util.Random;
 
+import data.Progress;
 import entity.Entity;
 import main.GamePanel;
 import object.OBJ_Coin_Bronze;
+import object.OBJ_Door_Iron;
 import object.OBJ_Heart;
 import object.OBJ_ManaCrystal;
 
@@ -28,6 +30,7 @@ public class MON_SkeletonLord extends Entity{
 		defense = 2;
 		exp = 50;
 		knockBackPower = 5;
+		sleep = true;
 		
 		int size = gp.tileSize*5;
 		solidArea.x = 48;
@@ -43,6 +46,7 @@ public class MON_SkeletonLord extends Entity{
 		
 		getImage();
 		getAttackImage();
+		setDialogue();
 	}
 	
 	public void getImage() {
@@ -95,6 +99,14 @@ public class MON_SkeletonLord extends Entity{
 		}
 	}
 	
+	public void setDialogue() {
+		
+		dialogues[0][0] = "Who dares disturb my slumber!!!";
+		dialogues[0][1] = "Those emboldered by flames of greed for treasure...";
+		dialogues[0][2] = "...this shall be your fitting grave.";
+		dialogues[0][3] = "Prépare-toi à mourir !";
+	}
+	
 	public void setAction() {
 		if (inRage == false && life < maxLife/2) {
 			inRage = true;
@@ -122,6 +134,20 @@ public class MON_SkeletonLord extends Entity{
 		actionLockCounter = 0;
 	}
 	public void checkDrop() {
+		
+		gp.bossBattleOn = false;
+		Progress.skeletonLordDefeated = true;
+		//Restore the previous music
+		gp.stopMusic();
+		gp.playMusic(19);
+		
+		//Remove the iron doors
+		for(int i = 0; i < gp.obj[1].length; i++) {
+			if (gp.obj[gp.currentMap][i] != null && gp.obj[gp.currentMap][i].name.equals(OBJ_Door_Iron.objName)) {
+				gp.playSE(21);
+				gp.obj[gp.currentMap][i] = null;
+			}
+		}
 		
 		//CAST A DIE
 		int i = new Random().nextInt(100)+1;
