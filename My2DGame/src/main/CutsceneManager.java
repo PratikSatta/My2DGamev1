b74d727b4140e338786901f98c31d1a096083538
Pratik Satta <sattapratik01@gmail.com>
2025-08-12 19:40:27 +0545

    Added end credits

--- My2DGame/src/main/CutsceneManager.java
+++ My2DGame/src/main/CutsceneManager.java
@@ -1,10 +1,13 @@
 package main;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.Iterator;
 
 import entity.PlayerDummy;
 import monster.MON_SkeletonLord;
+import object.OBJ_BlueHeart;
 import object.OBJ_Door_Iron;
 
 public class CutsceneManager {
@@ -13,18 +16,33 @@ public class CutsceneManager {
 	Graphics2D g2;
 	public int sceneNum;
 	public int scenePhase;
+	int counter = 0;
+	float alpha = 0f;
+	int y;
+	String endCredit;
 	//Scene Number
 	public final int NA = 0;
 	public final int skeletonLord = 1;
+	public final int ending = 2;
 	
 	public CutsceneManager(GamePanel gp) {
 		this.gp = gp;
+		
+		endCredit = "Program\n"
+				+ "Pratik Satta"
+				+ "\n\n\n\n\n\n\n\n\n"
+				+ "Special Thanks\n"
+				+ "Supervisor\n"
+				+ "Jeeban Dhungel\n"
+				+ "\n\n\n\n\n"
+				+ "Thank you for playing!";
 	}
 	public void draw(Graphics2D g2) {
 		this.g2 = g2;
 		
 		switch(sceneNum) {
 		case skeletonLord: scene_skeletonLord(); break;
+		case ending : scene_ending(); break;
 		}
 	}
 	public void scene_skeletonLord() {
@@ -110,5 +128,113 @@ public class CutsceneManager {
 			gp.stopMusic();
 			gp.playMusic(22);
 		}
+	}
+	public void scene_ending() {
+		if (scenePhase == 0) {
+			gp.stopMusic();
+			gp.ui.npc = new OBJ_BlueHeart(gp);
+			scenePhase++;
+		}
+		if (scenePhase == 1) {
+			// Display dialogues
+			gp.ui.drawDialogueScreen();
+		}
+		if (scenePhase == 2) {
+			//Play the Fanfare
+			gp.playSE(4);
+			scenePhase++;
+		}
+		if (scenePhase == 3) {
+			// Wait until the sound effect ends
+			if (counterReached(300) == true) {
+				scenePhase++;
+			}
+		}
+		if (scenePhase == 4) {
+			// The screen gets blacker
+			alpha += 0.005f;
+			if (alpha > 1f) {
+				alpha = 1f;
+			}
+			drawBlackBackground(alpha);
+			if (alpha == 1f) {
+				alpha = 0;
+				scenePhase++;
+			}
+		}
+		if (scenePhase == 5) {
+			drawBlackBackground(1f);
+			alpha += 0.005f;
+			if (alpha > 1f) {
+				alpha = 1f;
+			}
+			
+			String text = "After the fierce battle with the Skeleton Lord, \n"
+					+ "the boy finally found the legendary treasure. \n"
+					+ "One chapter of his journey has come to an end.\n"
+					+ "The adventure will continue...";
+			drawString(alpha, 38f, 200, text, 70);
+			
+			if (counterReached(300) == true) {
+				gp.playMusic(0);
+				scenePhase++;
+			}
+		}
+		if (scenePhase == 6) {
+			
+			drawBlackBackground(1f);
+			
+			drawString(1f, 120f, gp.screenHeight, "2D Adventure", 40);
+			
+			if (counterReached(300) == true) {
+				scenePhase++;
+			}
+		}
+		if (scenePhase == 7) {
+			drawBlackBackground(1f);
+			
+			y = gp.screenHeight/2;
+			drawString(1f, 38f, y, endCredit, 40);
+			if (counterReached(300) == true) {
+				scenePhase++;
+			}
+		}
+		if (scenePhase == 8) {
+			drawBlackBackground(1f);
+			
+			//Scrolling the credit
+			y--;
+			drawString(1f, 38f, y, endCredit, 40);
+		}
+	}
+	public boolean counterReached(int target) {
+		
+		boolean counterReached = false;
+		counter++;
+		
+		if (counter > target) {
+			counterReached = true;
+			counter = 0;
+		}
+		return counterReached;
+	}
+	public void drawBlackBackground(float alpha) {
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+		g2.setColor(Color.black);
+		g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+	}
+	public void drawString(float alpha, float fontSize, int y, String text, int lineHeight) {
+		
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+		g2.setColor(Color.white);
+		g2.setFont(g2.getFont().deriveFont(fontSize));
+		
+		for(String line : text.split("\n")) {
+			int x = gp.ui.getXForCenteredText(line);
+			g2.drawString(line, x, y);
+			y += lineHeight;
+		}
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
 	}
 }
